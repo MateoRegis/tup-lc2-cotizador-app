1936dfd60c1d176b34c5f7d1f8ae751bcb7fd3df
JensonMedina <medinajenson74@gmail.com>
2024-05-06 12:59:30 -0300

    Agregamos validacion al formulario de contacto

--- js/pages/contacto.js
+++ js/pages/contacto.js
@@ -1,26 +1,26 @@
+let nombre = document.getElementById("nombre");
+let email = document.getElementById("email");
+let mensaje = document.getElementById("mensaje");
+let lblValidacion = document.getElementById("lblValidacion");
 
-const btn_enviar = document.getElementById("btn-enviar");
-const btn_limpiar = document.getElementById("btn-limpiar");
-var respuesta = document.getElementById("respuesta");
-function enviar_formulario(){
-    let nombre = document.getElementById("nombre").value;
-    let email = document.getElementById("email").value;
-    let mensaje = document.getElementById("mensaje").value;
-    validar_formulario(nombre,email,mensaje);
-    console.log(nombre.value);
-    console.log(email.value);
-    console.log(mensaje.value);
+function EnviarFormulario() {
+  if (ValidarFormulario()) {
+    lblValidacion.innerHTML = "Todos los campos estan completos.";
+    lblValidacion.style.color = "Green";
+  } else {
+    lblValidacion.innerText = "Debes completar todos los campos.";
+    lblValidacion.style.color = "Red";
+  }
 }
-function validar_formulario(nombre,email,mensaje) {
-    
-    if (nombre === '' || email === '' || mensaje === '') {
-        respuesta.innerHTML = "Debes completar formularios";
-        alert('Falta completar el formulario. Por favor, asegúrate de rellenar todos los campos.');
-        return false;
-    }
-    else {
-        alert('ENVIO CORRECTO')
-        return true;
-    }
-    
+function LimpiarFormulario(){
+  nombre.value = "";
+  email.value = "";
+  mensaje.value = "";
+  lblValidacion.innerText = "";
+}
+function ValidarFormulario() {
+  if (nombre.value != "" && email.value != "" && mensaje.value != "") {
+    return true;
+  }
+  return false;
 }
