c568499446b947505cee65d75f967018bb9bb3b1
JensonMedina <medinajenson74@gmail.com>
2024-05-29 14:45:16 -0300

    Esta rama es para empezar a hacer las pruebas de js

--- js/common.js
+++ js/common.js
@@ -1,3 +1,110 @@
+//voy a agregar el menu directamente desde el js a todas las paginas
+let header = document.getElementById("header");
+let menu = `<div id="menu">
+<div class="icono-container">
+  <a href="index.html"><img src="img/bullCoinIcono.jpg" alt="" /></a>
+</div>
+<ul>
+  <li>
+    <a href="index.html"><i class="fa-solid fa-house"></i> Inicio</a>
+  </li>
+  <li>
+    <a href="Miarchivo.html"
+      ><i class="fa-solid fa-folder-open"></i> Mi Archivo</a
+    >
+  </li>
+  <li>
+    <a href="informes.html"
+      ><i class="fa-solid fa-file"></i> Informes</a
+    >
+  </li>
+  <li>
+    <a href="contacto.html"
+      ><i class="fa-solid fa-address-book"></i> Contáctenos</a
+    >
+  </li>
+</ul>
+</div>
+<button class="btn-mnu" onclick="eventoMenu()" id="btnMnu">≡</button>`;
+
+header.innerHTML = menu;
+
+//el footer tambien lo voy a agregar mediante js
+let footer = document.getElementById("footer");
+let footerContent = `<div class="footer-container">
+<div class="institucional-container item-footer">
+  <h3>INSTITUCIONAL</h3>
+  <ul class="lista-institucional">
+    <li class="lista-institucional-item">
+      <a href="">¿Quiénes Somos?</a>
+    </li>
+    <li class="lista-institucional-item">
+      <a href="">Socios Estratégicos</a>
+    </li>
+    <li class="lista-institucional-item">
+      <a href="">Trabaje con Nosotros</a>
+    </li>
+    <li class="lista-institucional-item"><a href="">Ayuda</a></li>
+  </ul>
+</div>
+<div class="accesos-container item-footer">
+  <h3>ACCESOS</h3>
+  <ul class="lista-accesos">
+    <li class="lista-accesos-item"><a href="index.html">Inicio</a></li>
+    <li class="lista-accesos-item">
+      <a href="Miarchivo.html">Mi Archivo</a>
+    </li>
+    <li class="lista-accesos-item">
+      <a href="informes.html">Informes</a>
+    </li>
+    <li class="lista-accesos-item">
+      <a href="contacto.html">Contáctenos</a>
+    </li>
+  </ul>
+</div>
+<div class="contacto-container item-footer">
+  <h3>CONTACTO</h3>
+  <ul class="lista-contacto">
+    <li class="lista-contacto-item">
+      Universidad Tecnológica Nacional
+    </li>
+    <li class="lista-contacto-item">
+      ZEBALLOS 1341 - S2000BQA - ROSARIO
+    </li>
+    <li class="lista-contacto-item">0341 - 4481871</li>
+    <li class="lista-contacto-item">
+      <a href="contacto.html">!Escríbanos!</a>
+    </li>
+  </ul>
+</div>
+<div class="desarrollado-container item-footer">
+  <h3>DESARROLLADO POR</h3>
+  <ul class="lista-desarrollo">
+    <li class="lista-desarrollo-item">
+      <a href="https://github.com/JensonMedina" target="_blank"
+        >Jenson Medina (53338)</a
+      >
+    </li>
+    <li class="lista-desarrollo-item">
+      <a href="https://github.com/MateoRegis" target="_blank"
+        >Mateo Regis (48963)</a
+      >
+    </li>
+    <li class="lista-desarrollo-item">
+      <a href="https://github.com/BrunoRubini" target="_blank"
+        >Bruno Rubini(53367)</a
+      >
+    </li>
+    <li class="lista-desarrollo-item">TUP06 - 2024</li>
+  </ul>
+</div>
+</div>
+<div class="nombre-materia">
+<span>Tup - Cátedra de Laboratorio de Computación 2 - Año 2024</span>
+</div>`;
+footer.innerHTML = footerContent;
+
+
 /**************************************************/
 /* Evento menu desplegable  */
 /**************************************************/
--- js/pages/index.js
+++ js/pages/index.js
@@ -1,33 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {               //funcion que se ejecutara cuando el evento DOMContentLoaded ocurra
-  
-  const imagenes = [                                                          // Arreglo de imágenes que se mostrarán en el fondo
-    'url("img/fondo1.jpg")',
-    'url("img/fondo2.jpg")',
-    'url("img/fondo3.jpg")',
-    'url("img/fondo4.jpg")',
-    'url("img/fondo5.jpg")',
-    'url("img/fondo6.jpg")',
-  ];
+//me traigo el boton de selector moneda
 
- 
-  let indiceActual = 0;                                                    // Índice inicial para el arreglo de imágenes
-  
-  const elementoFondo = document.getElementById("BackgroundDinamico");    // Elemento del DOM cuyo fondo se actualizará
-
-  // Función para actualizar la imagen de fondo
-  function actualizarFondo() {
-    elementoFondo.style.opacity = "0";                                    //Establece la opacidad del fondo a 0 (transparente)
-
-    // Espera a que la transición de opacidad termine antes de cambiar la imagen
-    setTimeout(() => {
-      elementoFondo.style.backgroundImage = imagenes[indiceActual];    // Cambia la imagen de fondo
-      elementoFondo.style.opacity = "1";                               // Establece la opacidad a 1 (visible)
-    }, 2000);                                                         // Tiempo de espera corresponde al tiempo de la transición de opacidad
-  }
-
-  // Establece un intervalo para cambiar las imágenes automáticamente
-  setInterval(() => {
-    indiceActual = (indiceActual + 1) % imagenes.length;   // Actualiza el índice al siguiente, y vuelve al inicio después de la última
-    actualizarFondo();                                    // Llama a la función para actualizar el fondo
-  }, 7000);                                              // Tiempo total del intervalo incluyendo la transición (2s de desvanecimiento + 5s de imagen visible)
-});
+let btnSelectorMoneda = document.getElementById("btn-selector-moneda");
